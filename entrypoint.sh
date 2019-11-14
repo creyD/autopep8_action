57240f587faa62b518ea6870ab13ef2a29706ffc
Conrad <grosserconrad@gmail.com>
2019-11-14 23:55:03 +0100

    Added action code

---
+++ entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -l
+
+set -eu
+
+# Function for setting up git env in the docker container (copied from https://github.com/stefanzweifel/git-auto-commit-action/blob/master/entrypoint.sh)
+git_setup ( ) {
+  cat <<- EOF > $HOME/.netrc
+        machine github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+        machine api.github.com
+        login $GITHUB_ACTOR
+        password $GITHUB_TOKEN
+EOF
+    chmod 600 $HOME/.netrc
+
+    git config --global user.email "actions@github.com"
+    git config --global user.name "GitHub Actions"
+}
+
+echo "Installing dependencies..."
+pip install --upgrade pip
+# Install dependencies
+pip install autopep8
+
+# Install custom project dependencies if applicable
+if ! $INPUT_DEPENDENCIES == "false"
+then
+  pip install -r $INPUT_DEPENDENCIES
+fi
+
+# Apply PEP 8
+echo "Running autopep8..."
+autopep8 --in-place -r .
+
+if ! git diff --quiet
+then
+  echo "Commiting and pushing changes."
+  # Calling method to configure the git environemnt
+  git_setup
+  # Switch to the actual branch
+  git checkout $INPUT_BRANCH
+
+  git add "${INPUT_FILE_PATTERN}"
+
+  git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
+  git push --set-upstream origin "HEAD:$INPUT_BRANCH"
+else
+  echo "Nothing to commit. Exiting."
+fi
