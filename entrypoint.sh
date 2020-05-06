3f07e64ccbe6d074c8424411d482c4f3a04d0ac0
Conrad <grosserconrad@gmail.com>
2020-05-06 12:34:50 +0200

    Removed -l command from the shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 # e is for exiting the script automatically if a command fails, u is for exiting if a variable is not set
 # x would be for showing the commands before they are executed
 set -eu
