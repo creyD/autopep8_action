c8d9e2fc74c2a7a3bdb22c6a3c94aacc88ef6574
Conrad <grosserconrad@gmail.com>
2020-05-06 12:30:39 +0200

    Fixed the git status bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,12 +20,16 @@ EOF
     git config --global user.name "GitHub Action"
 }
 
+# Checks if any files are changed
+_git_changed() {
+    [[ -n "$(git status -s)" ]]
+}
+
 echo "Running autopep8..."
 autopep8 -i -r $INPUT_CHECKPATH $INPUT_OPTIONS || echo "Problem running autopep8!"
 
-git status -s
-
-if [-n "$(git status -s)"]; then
+if _git_changed;
+then
   if $INPUT_DRY; then
     echo "Found non-compliant files!"
     exit 1
